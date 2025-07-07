f3b6521306aede7cc0e694e5999cc6bde4d70f0f
0822-Ka <alfaesmirnainfanteo8@gmail.com>
2025-07-07 17:39:34 +0200

    Add files via upload

---
+++ INDEX_resources/js/ardoraMorFeriCFG.js
@@ -0,0 +1,32 @@
+//Creado con Ardora - www.webardora.net
+//bajo licencia Attribution-NonCommercial-NoDerivatives 4.0 International (CC BY-NC-ND 4.0)
+//para otros usos contacte con el autor
+var timeAct=360; timeIni=360; timeBon=0;
+var successes=0; successesMax=5; attempts=0; attemptsMax=5;
+var score=0; scoreMax=5; scoreInc=1; scoreDec=0
+var typeGame=0;
+var tiTime=false;
+var tiTimeType=0;
+var tiButtonTime=true;
+var textButtonTime="Comenzar";
+var tiSuccesses=false;
+var tiAttempts=false;
+var tiScore=false;
+var startTime;var tiAudio=false;
+var colorBack="#FFFDFD"; colorButton="#91962F"; colorText="#000000"; colorSele="#FF8000";
+var goURLNext=false; goURLRepeat=false;tiAval=false;
+var scoOk=0; scoWrong=0; scoOkDo=0; scoWrongDo=0; scoMessage=""; scoPtos=10;
+var fMenssage="Verdana, Geneva, sans-serif";
+var fActi="Verdana, Geneva, sans-serif";
+var fEnun="Verdana, Geneva, sans-serif";
+var timeOnMessage=5; messageOk=""; messageTime=""; messageError=""; messageErrorG=""; messageAttempts=""; isShowMessage=false;
+var urlOk=""; urlTime=""; urlError=""; urlAttempts="";
+var goURLOk="_blank"; goURLTime="_blank"; goURLAttempts="_blank"; goURLError="_blank"; 
+borderOk="#008000"; borderTime="#FF0000";borderError="#FF0000"; borderAttempts="#FF0000";
+var wordsGame="SU5ERVg="; wordsStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+function giveZindex(typeElement){var valueZindex=0; capas=document.getElementsByTagName(typeElement);
+for (i=0;i<capas.length;i++){if (parseInt($(capas[i]).css("z-index"),10)>valueZindex){valueZindex=parseInt($(capas[i]).css("z-index"),10);}}return valueZindex;}
+
+var col=0; row=0; writeDir=0;
+var words=["SU5URVJORVQ=","TUFFU1RSQQ==","SURFQVM=","Rk9UT1M=","Q09MQUJPUkFS"];var defs=["Red global que permite conectar dispositivos","Persona que nos enseña a usar Padlet y Jamboard.","Lo que compartimos en Padlet con los demás.","Se pueden subir al muro o a la pizarra digital.","Trabajar con otros en una misma pizarra virtual."];
+var indexWords=0;var indexAttemps=0;
